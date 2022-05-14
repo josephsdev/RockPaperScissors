9390dbbe84d04d5b68fc91795af6226277d053cb
josephsdev <joseph@sdev.ca>
2022-05-14 16:10:37 -0700

    comp select working

---
+++ RPS.js
@@ -0,0 +1,13 @@
+function computerPicks () {
+    let compChoices = ["rock", "paper", "scissors"]
+    let randomizeChoice = Math.floor(Math.random() * compChoices.length) + 1;
+
+    if (randomizeChoice === 1) {
+    return "rock";
+    }  if (randomizeChoice === 2) {
+        return "paper";
+    }  if (randomizeChoice === 3) {
+        return "scissors";
+    }
+}
+console.log(computerPicks());
