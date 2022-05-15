754e9a91d3bc6fca30b28d39d17eef176e008c92
josephsdev <joseph@sdev.ca>
2022-05-14 20:24:23 -0700

    not working but progress

--- RPS.js
+++ RPS.js
@@ -1,4 +1,4 @@
-function computerPicks () {
+function computerPlay () {
     let compChoices = ["rock", "paper", "scissors"]
     let randomizeChoice = Math.floor(Math.random() * compChoices.length) + 1;
 
@@ -10,8 +10,41 @@ function computerPicks () {
         return "scissors";
     }
 }
-function playerPicks () {
+
+function playa () {
     var i = prompt("Rock, Paper, or Scissors?:");
     return i.toLowerCase();
 }
-console.log(computerPicks(), playerPicks())
+console.log(computerPlay(), playa())
+
+function PlayAround (computerPlay, playerPicks) {
+    var computerPicks = computerPlay ()  
+    var playerPicks = playa()
+    if (playerPicks === computerPicks) {
+        console.log("It's a tie!")
+        }
+        else if  (playerPicks == ("rock")) {
+        if (computerPicks == ("paper")) {
+        console.log ("You lose!")
+        }
+        else if (computerPicks == ("scissors")) {
+            alert("You win!")
+        } }
+        else if  (playerPicks == ("paper")) {
+        if (computerPicks == ("scissors")) {
+            alert("You lose!")
+        }
+        else if (computerPicks == ("rock")) {
+            alert("You win!")
+        } }
+        else if  (playerPicks == ("scissors")) { 
+        if (computerPicks == ("paper")) {
+            alert("You Win!")
+    }
+        else if (computerPicks == ("rock")) {
+            alert("You lose!")
+        }      
+} }
+
+    
+
